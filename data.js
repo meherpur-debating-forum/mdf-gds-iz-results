9962cc8500b13345c379a6359335c8dc3c4c8db8
meherpur-debating-forum <meherpurdebatingforum@gmail.com>
2026-03-28 13:21:26 +0600

    data.js

--- data.js
+++ data.js
@@ -1,12 +1,1 @@
-// data.js
-const allParticipants = [
-    { name: "আব্দুর রহমান", phone: "01711111111", institute: "ঢাকা কলেজ", segment: "কবিতা লিখন", mark: ৯৫ },
-    { name: "জান্নাতুল ফেরদৌস", phone: "01822222222", institute: "রাজশাহী স্কুল", segment: "চিত্রাঙ্কন", mark: ৮৬ },
-    { name: " ইসতিয়াক আহমেদ", phone: "01644547079", institute: "মেহেরপুর সরকারি কলেজ", segment: "কবিতা লিখন", mark: ৯৫ },
-    { name: "ইসতিয়াক আহমেদ", phone: "01644547079", institute: "মেহেরপুর সরকারি কলেজ", segment: "চিত্রাঙ্কন", mark: ৯৫ },
-    { name: "সাকিব আল হাসান", phone: "01933333333", institute: "আইডিয়াল স্কুল", segment: "গ্রাফিতি", mark: ৯২ },
-    { name: "সাদিয়া ইসলাম", phone: "01511111111", institute: "নটর ডেম কলেজ", segment: "বারোয়ারী বিতর্ক", mark: ৯৮ },
-    { name: "তানভীর আহমেদ", phone: "01711111112", institute: "রাজশাহী স্কুল", segment: "কবিতা লিখন", mark: ৯০ },
-    { name: "ফারিয়া সুলতানা, হ্যাবলা, কানচনমালা, আম", phone: "01822222223", institute: "ভিকারুননিসা", segment: "গ্রাফিতি", mark: ৮৮ },
-    // আরও ডাটা যোগ করতে থাকুন...
-];
+const allParticipants = [];
